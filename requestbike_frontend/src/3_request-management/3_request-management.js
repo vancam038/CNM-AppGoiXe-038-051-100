a2d70cfb9934c252062e7db124a04b6116fdaf1f
vancam038 <1512038@student.hcmus.edu.vn>
2018-11-13 06:01:58 +0700

    Fix bug responsive dropdown driver

--- requestbike_frontend/src/3_request-management/3_request-management.js
+++ requestbike_frontend/src/3_request-management/3_request-management.js
@@ -59,6 +59,36 @@ $(function () {
             }
         });
     })
-
-
 });
+
+function initMap() {
+
+    /// google map start
+    var myMap, marker;
+
+    var geocode = {
+        lat: 10.762622,
+        lng: 106.660172
+    };
+
+    myMap = new google.maps.Map(document.getElementById('map'), {
+        center: geocode,
+        zoom: 12,
+        scrollwheel: false
+    });
+
+    marker = new google.maps.Marker({
+        position: geocode,
+        map: myMap,
+        animation: google.maps.Animation.BOUNCE,
+        icon: {
+            url: '../../assets/img/man.png',
+            scaledSize: {
+                width: 50,
+                height: 50
+            }
+        }
+    });
+
+    // google map end
+};
