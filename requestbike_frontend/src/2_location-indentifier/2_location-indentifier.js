9df2f7483e9968c81491d5d9fbd242cd5734e6d8
trhdon123 <trhdon123@gmail.com>
2018-11-14 20:53:26 +0700

    fix script app#2

--- requestbike_frontend/src/2_location-indentifier/2_location-indentifier.js
+++ requestbike_frontend/src/2_location-indentifier/2_location-indentifier.js
@@ -1,6 +1,6 @@
 var socket = io("http://localhost:3001");
 
-$(function () {
+$(function() {
   let dataTable = null;
   dataTable = $("#reqTable").DataTable({
     paging: false,
@@ -11,89 +11,45 @@ $(function () {
     }
   });
 
-  // $.ajax({
-  //     url: 'http://localhost:3000/requests',
-  //     type: 'GET',
-  //     dataType: 'json',
-  //     timeout: 10000
-  // }).done(function (data) {
-  //     var source = document.getElementById("request-template").innerHTML;
-  //     var template = Handlebars.compile(source);
-  //     var html = template(data);
-  //     $('#requests').html(html);
-  //     dataTable = $('#reqTable').DataTable({
-  //         "paging": true,
-  //         "lengthChange": false
-  //     });
-  // })
-
-  // 1_to_2_transfer-req
-  socket.on("1_to_2_transfer-req", reqs => {
-    if (dataTable) {
-      dataTable.destroy();
-      dataTable = null;
-    }
-    var source = document.getElementById("request-template").innerHTML;
-    var template = Handlebars.compile(source);
-    var html = template(reqs);
-    $("#requests").html(html);
-
-    dataTable = $("#reqTable").DataTable({
-      paging: false,
-      scrollY: 350,
-      lengthChange: false,
-      info: true,
-      language: {
-        info: "Total: _TOTAL_ requests"
-      },
-      createdRow: function(row, data, index) {
-        // console.log(row, data, index);
-        const btn_locate = $("button.btn-locate", row)[0];
-        if (btn_locate)
-          $(btn_locate).click(function() {
-            const tr = $(btn_locate).closest("tr")[0];
-            const reqId = $(tr).attr("data-id"),
-              lat = $(tr).attr("data-lat"),
-              lng = $(tr).attr("data-lng");
-            if (reqId === undefined || lat === undefined || lng === undefined)
-              return;
-            prevLatLng = new google.maps.LatLng(lat, lng);
-            handleQueryGeolocationFinish(prevLatLng);
-            // $.get(`http://localhost:3000/request/coords/${reqId}`)
-            //   .then(response => response.json())
-            //   .then(result => {
-            //     const { status, coords } = result;
-            //     if (status === "OK") {
-            //       drawUserMarker(coords);
-            //     } else {
-            //       // message staff request not found
-            //     }
-            //   })
-            //   .catch(err => {
-            //     // message staff server error
-            //   });
-          });
-        // const btn_find = $("button.btn-find", row);
-        // console.log("btn_find", btn_find);
-        // btn_find.onClick(() => {
-        // const reqId = btn
-        //   .parent()
-        //   .parent()
-        //   .children(":first").innerHTML;
-        // console.log(reqId);
-        // // disable button while server finding driver
-        // btn.attr("disabled", true);
-        // // $.get(`http://localhost:3000/getReqCoords/${reqId}`)
-        // //   .then(response => response.json())
-        // //   .then(result => {
-        // //     const { status } = result;
-        // //     if (status !== "OK") {
-        // //     }
-        // //   })
-        // //   .catch(err => {});
-        // console.log(index);
-        // });
+  socket.on("new_request_added", () => {
+    $.ajax({
+      url: "http://localhost:3000/request/unidentified",
+      type: "GET",
+      dataType: "json",
+      timeout: 10000
+    }).done(function(data) {
+      if (dataTable) {
+        dataTable.destroy();
+        dataTable = null;
       }
+      var source = document.getElementById("request-template").innerHTML;
+      var template = Handlebars.compile(source);
+      var html = template(data);
+      $("#requests").html(html);
+      dataTable = $("#reqTable").DataTable({
+        paging: false,
+        scrollY: 350,
+        lengthChange: false,
+        info: true,
+        language: {
+          info: "Total: _TOTAL_ requests"
+        },
+        createdRow: function(row) {
+          const btn_locate = $("button.btn-locate", row)[0];
+          if (btn_locate)
+            $(btn_locate).click(function() {
+              const tr = $(btn_locate).closest("tr")[0];
+              const reqId = $(tr).attr("data-id"),
+                lat = $(tr).attr("data-lat"),
+                lng = $(tr).attr("data-lng");
+              console.log(reqId, lat, lng);
+              if (reqId === undefined || lat === undefined || lng === undefined)
+                return;
+              prevLatLng = new google.maps.LatLng(lat, lng);
+              handleQueryGeolocationFinish(prevLatLng);
+            });
+        }
+      });
     });
   });
 });
