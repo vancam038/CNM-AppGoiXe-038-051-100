4321a9cd71c35b7033a517bc64f46c3c758d8a75
trhdon123 <trhdon123@gmail.com>
2018-11-14 21:07:33 +0700

    minor changes on app#2

--- requestbike_frontend/src/2_location-indentifier/2_location-indentifier.js
+++ requestbike_frontend/src/2_location-indentifier/2_location-indentifier.js
@@ -1,56 +1,52 @@
 var socket = io("http://localhost:3001");
 
+function getUnidentifiedReqs(dataTable) {
+  $.ajax({
+    url: "http://localhost:3000/request/unidentified",
+    type: "GET",
+    dataType: "json",
+    timeout: 10000
+  }).done(function(data) {
+    if (dataTable) {
+      dataTable.destroy();
+      dataTable = null;
+    }
+    var source = document.getElementById("request-template").innerHTML;
+    var template = Handlebars.compile(source);
+    var html = template(data);
+    $("#requests").html(html);
+    dataTable = $("#reqTable").DataTable({
+      paging: false,
+      scrollY: 350,
+      lengthChange: false,
+      info: true,
+      searching: false,
+      language: {
+        info: "Total: _TOTAL_ requests"
+      },
+      createdRow: function(row) {
+        const btn_locate = $("button.btn-locate", row)[0];
+        if (btn_locate)
+          $(btn_locate).click(function() {
+            const tr = $(btn_locate).closest("tr")[0];
+            const reqId = $(tr).attr("data-id"),
+              lat = $(tr).attr("data-lat"),
+              lng = $(tr).attr("data-lng");
+            if (reqId === undefined || lat === undefined || lng === undefined)
+              return;
+            prevLatLng = new google.maps.LatLng(lat, lng);
+            handleQueryGeolocationFinish(prevLatLng);
+          });
+      }
+    });
+  });
+}
+
 $(function() {
   let dataTable = null;
-  dataTable = $("#reqTable").DataTable({
-    paging: false,
-    lengthChange: false,
-    info: false,
-    language: {
-      emptyTable: "Hiện chưa có request mới nào được submit"
-    }
-  });
-
+  getUnidentifiedReqs(dataTable);
   socket.on("new_request_added", () => {
-    $.ajax({
-      url: "http://localhost:3000/request/unidentified",
-      type: "GET",
-      dataType: "json",
-      timeout: 10000
-    }).done(function(data) {
-      if (dataTable) {
-        dataTable.destroy();
-        dataTable = null;
-      }
-      var source = document.getElementById("request-template").innerHTML;
-      var template = Handlebars.compile(source);
-      var html = template(data);
-      $("#requests").html(html);
-      dataTable = $("#reqTable").DataTable({
-        paging: false,
-        scrollY: 350,
-        lengthChange: false,
-        info: true,
-        language: {
-          info: "Total: _TOTAL_ requests"
-        },
-        createdRow: function(row) {
-          const btn_locate = $("button.btn-locate", row)[0];
-          if (btn_locate)
-            $(btn_locate).click(function() {
-              const tr = $(btn_locate).closest("tr")[0];
-              const reqId = $(tr).attr("data-id"),
-                lat = $(tr).attr("data-lat"),
-                lng = $(tr).attr("data-lng");
-              console.log(reqId, lat, lng);
-              if (reqId === undefined || lat === undefined || lng === undefined)
-                return;
-              prevLatLng = new google.maps.LatLng(lat, lng);
-              handleQueryGeolocationFinish(prevLatLng);
-            });
-        }
-      });
-    });
+    getUnidentifiedReqs(dataTable);
   });
 });
 
