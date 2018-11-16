22db5ff1b80faf762f9a1bcec6f0804d4b7f3748
ApolloRisky <quoccuong.nguyen265@hotmail.com>
2018-11-16 22:00:43 +0700

    + Set Cookie (NOT YET)

--- requestbike_frontend/src/0_login/0_login.js
+++ requestbike_frontend/src/0_login/0_login.js
@@ -1,5 +1,37 @@
 $(function () {
-    var type = null;
+    var type = -1;
+    let onSuccess = function(data){
+        console.log(data);
+        if(data.auth) {
+            let cookie = {
+                "x-access-token": data.access_token,
+                "ref-token": data.refresh_token,
+                // "expires": data.expires
+            };
+            let cookieStr = JSON.stringify(cookie);
+            //$.cookie("com.requestbike-ss.app", JSON.stringify(cookie));
+            setCookie("com.requestbike-ss.app",cookieStr,data.expires);
+            console.log("Login Success!");
+            $('#alert-success').show();
+        }else{
+            console.log('Login Fail');
+            $('#alert-danger').show();
+        }
+    };
+
+    let onError = function(){
+        console.log('No connection!');
+        $('#alert-danger').show();
+    };
+
+    let setCookie = function(name,value,expTime){
+        var d = new Date();
+        d.setTime(d.getTime() + (expTime*1000)); //in miliseconds
+        var expires = d.toUTCString();
+        // document.cookie = `${name}=${value};exprires=${expires};path=/`;
+        $.cookie(`${name}=${value};exprires=${expires};path=/`);
+    };
+    /////////////
     $('.role-option').on('click', function(){
         var index = $(this).index();
         console.log('selected: ' + index);
@@ -20,7 +52,7 @@ $(function () {
             username,
             pwd,
             type
-        }
+        };
         $.ajax({
             url:'http://localhost:3000/user/login',
             type:'POST',
@@ -30,13 +62,8 @@ $(function () {
             },
             data: JSON.stringify(reqObject),
             dataType:'json',
-            success: function(data){
-                console.log(data);
-            },
-            error:function(){
-                console.log('Login Fail');
-                $('#alert-danger').show();
-            }
+            success: onSuccess,
+            error:onError
         })
     });
 });
