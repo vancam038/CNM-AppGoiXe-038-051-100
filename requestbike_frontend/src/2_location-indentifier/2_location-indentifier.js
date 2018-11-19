8bb6337ddee23e6c5711eff4919952ce5c915d69
vancam038 <1512038@student.hcmus.edu.vn>
2018-11-19 23:37:41 +0700

    Refactor code

--- requestbike_frontend/src/2_location-indentifier/2_location-indentifier.js
+++ requestbike_frontend/src/2_location-indentifier/2_location-indentifier.js
@@ -120,3 +120,47 @@ function keepSelectedRow() {
     }
   })
 }
+
+function setStatusByReqId(tableId, idReq, status) {
+  const tableId_string = '#' + tableId;
+  const reqId_string = '#' + idReq;
+  $(tableId_string + ' tr').each(function () {
+    const reqId = $(reqId_string).val();
+    const reqId_table = $(this).find("td:first").html();
+    const reqObject = {
+      reqId,
+      status
+    };
+    if (reqId_table === reqId) {
+      // Đầu tiên, cập nhật status của nó dưới db
+      $.ajax({
+        url: "http://localhost:3000/request/status",
+        type: "PATCH",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+        data: JSON.stringify(reqObject),
+        dataType: "json",
+      }).done(function () {
+        // sau khi cập nhật thành công thì reload lại table (query db để ghi đè lên lại)
+        // App#2 cũng phải tự realtime vs chính nó (nhiều app#2)
+        socket.on("2_to_2_reload_table")
+        $.ajax({
+          url: "http://localhost:3000/requests/unidentified+identified",
+          type: "GET",
+          dataType: "json"
+        }).done(function (data) {
+          var source = document.getElementById("request-template").innerHTML;
+          var template = Handlebars.compile(source);
+          var html = template(data);
+          $("#requests").html(html);
+          // đồng thời emit cho app#3 biết, để cùng realtime
+          socket.emit("2_to_3_reload-table");
+        });
+
+        socket.emit("2_to_2_reload-table");
+      });
+    }
+  });
+}
