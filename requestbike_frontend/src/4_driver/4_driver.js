66fc3df802633f39d1eb06bfbebc450167d5f777
trhdon123 <trhdon123@gmail.com>
2018-11-23 20:54:50 +0700

    done flow START & FINISH journey

--- requestbike_frontend/src/4_driver/4_driver.js
+++ requestbike_frontend/src/4_driver/4_driver.js
@@ -1,227 +1,276 @@
 var socket = io("http://localhost:3001");
 let infoWindow = null;
 
-$(function () {
-    $("#requestModalCenter").modal({
-        backdrop: "static",
-        // mặc định khi init, sẽ show modal. Nếu ko mún show thì chỉnh thành false
-        show: false
-    });
+$(function() {
+  $("#requestModalCenter").modal({
+    backdrop: "static",
+    // mặc định khi init, sẽ show modal. Nếu ko mún show thì chỉnh thành false
+    show: false
+  });
 });
 
 function updateDriverStatus(status, driverId) {
-    const driverObject = {
-        driverId,
-        status
-    };
-    // Đầu tiên, cập nhật status của nó dưới db
-    $.ajax({
-        url: "http://localhost:3000/driver/status",
-        type: "PATCH",
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json"
-        },
-        data: JSON.stringify(driverObject),
-        dataType: "json",
-    }).done(function () {
-        // socket start
-        // socket end
-    });
+  const driverObject = {
+    driverId,
+    status
+  };
+  // Đầu tiên, cập nhật status của nó dưới db
+  $.ajax({
+    url: "http://localhost:3000/driver/status",
+    type: "PATCH",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    },
+    data: JSON.stringify(driverObject),
+    dataType: "json"
+  }).done(function() {
+    // socket start
+    // socket end
+  });
 }
 
 function changeStatus(status) {
-    switch (status) {
-        case DRIVER_STATUS_READY:
-            $("#navbarDropdown").html(DRIVER_STATUS_READY);
-            $("#navbarDropdown")
-                .removeClass("btn-outline-danger btn-outline-warning")
-                .addClass("btn-outline-success");
-            // ajax cập nhật status của tài xế thành ready
-            // updateDriverStatus(DRIVER_STATUS_READY, "tAJ1PUTcaf"); // TESTING
-            break;
-        case DRIVER_STATUS_STANDBY:
-            $("#navbarDropdown").html(DRIVER_STATUS_STANDBY);
-            $("#navbarDropdown")
-                .removeClass("btn-outline-success btn-outline-danger")
-                .addClass("btn-outline-warning");
-            //socket start
-            //socket end
+  switch (status) {
+    case DRIVER_STATUS_READY:
+      $("#navbarDropdown").html(DRIVER_STATUS_READY);
+      $("#navbarDropdown")
+        .removeClass("btn-outline-danger btn-outline-warning")
+        .addClass("btn-outline-success");
+      // ajax cập nhật status của tài xế thành ready
+      // updateDriverStatus(DRIVER_STATUS_READY, "tAJ1PUTcaf"); // TESTING
+      break;
+    case DRIVER_STATUS_STANDBY:
+      $("#navbarDropdown").html(DRIVER_STATUS_STANDBY);
+      $("#navbarDropdown")
+        .removeClass("btn-outline-success btn-outline-danger")
+        .addClass("btn-outline-warning");
+      //socket start
+      //socket end
 
-            // ajax cập nhật status của tài xế thành standby
-            // updateDriverStatus(DRIVER_STATUS_STANDBY, "tAJ1PUTcaf"); // TESTING
-            break;
-            //etc...
-        case DRIVER_STATUS_BUSY: // for testing
-            $("#navbarDropdown").html(DRIVER_STATUS_BUSY);
-            $("#navbarDropdown")
-                .removeClass("btn-outline-success btn-outline-warning")
-                .addClass("btn-outline-danger");
-            //socket start
-            //socket end
+      // ajax cập nhật status của tài xế thành standby
+      // updateDriverStatus(DRIVER_STATUS_STANDBY, "tAJ1PUTcaf"); // TESTING
+      break;
+    //etc...
+    case DRIVER_STATUS_BUSY: // for testing
+      $("#navbarDropdown").html(DRIVER_STATUS_BUSY);
+      $("#navbarDropdown")
+        .removeClass("btn-outline-success btn-outline-warning")
+        .addClass("btn-outline-danger");
+      //socket start
+      //socket end
 
-            // ajax cập nhật status của tài xế thành ready
-            break;
-    }
+      // ajax cập nhật status của tài xế thành ready
+      break;
+  }
 }
 
 function updateReqStatus(reqId) {
-    const status = REQ_STATUS_ACCEPTED;
-    const reqObject = {
-        reqId,
-        status
-    };
+  const status = REQ_STATUS_ACCEPTED;
+  const reqObject = {
+    reqId,
+    status
+  };
 
-    // Đầu tiên, cập nhật status của nó dưới db
-    $.ajax({
-        url: "http://localhost:3000/request/status",
-        type: "PATCH",
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json"
-        },
-        data: JSON.stringify(reqObject),
-        dataType: "json",
-    }).done(function () {
-        // emit cho 3 là đã có xe nhận -> reload lại table
-        socket.emit("4_to_3_reload-table");
-        // emit cho 2 là đã có xe nhận -> reload lại table -> mất req identified bên #2
-        socket.emit("4_to_2_reload-table");
-    });
+  // Đầu tiên, cập nhật status của nó dưới db
+  $.ajax({
+    url: "http://localhost:3000/request/status",
+    type: "PATCH",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    },
+    data: JSON.stringify(reqObject),
+    dataType: "json"
+  }).done(function() {
+    // emit cho 3 là đã có xe nhận -> reload lại table
+    socket.emit("4_to_3_reload-table");
+    // emit cho 2 là đã có xe nhận -> reload lại table -> mất req identified bên #2
+    socket.emit("4_to_2_reload-table");
+  });
 }
 
 // lat:  reqLat, lng: reqLng, addr: reqAddr
 function updateMap(lat, lng, addr) {
-    // đóng infoWindow trước đó
-    if (infoWindow) infoWindow.close();
-    // đóng passengerMarker trước đó
-    if (passengerMarker) passengerMarker.setMap(null);
+  // đóng infoWindow trước đó
+  if (infoWindow) infoWindow.close();
+  // đóng passengerMarker trước đó
+  if (passengerMarker) passengerMarker.setMap(null);
 
-    // driverMarker sẽ ko thể drag nữa
-    driverMarker.setDraggable(false);
+  // driverMarker sẽ ko thể drag nữa
+  driverMarker.setDraggable(false);
 
-    passengerLatLng = new google.maps.LatLng(lat, lng);
-    drawPassengerMarker(passengerLatLng);
-    drawPathDriverToPassenger(prevLatLng, passengerLatLng);
-    // show thông tin hành khách
-    infoWindow = new google.maps.InfoWindow({
-        content: `<span style="font-weight:bold">${addr}</span>`
-    });
-    infoWindow.open(map, passengerMarker);
+  passengerLatLng = new google.maps.LatLng(lat, lng);
+  drawPassengerMarker(passengerLatLng);
+  drawPathDriverToPassenger(prevLatLng, passengerLatLng);
+  // show thông tin hành khách
+  infoWindow = new google.maps.InfoWindow({
+    content: `<span style="font-weight:bold">${addr}</span>`
+  });
+  infoWindow.open(map, passengerMarker);
 }
 
 //socket start
 
 //listen start
-$(function () {
-    // gửi trạng thái lên cho server
-    // socket.emit("4_to_2_???", requestObject);
+$(function() {
+  // gửi trạng thái lên cho server
+  // socket.emit("4_to_2_???", requestObject);
 
-    // lắng nghe yêu cầu từ phía #2
-    var timer = new Timer();
-    socket.on("2_to_4_send-req-to-driver", msg => {
-        const {
-            reqId,
-            lat,
-            lng,
-            addr
-        } = JSON.parse(msg);
-        if (reqId === undefined || lat === undefined || lng === undefined) return;
+  // lắng nghe yêu cầu từ phía #2
+  var timer = new Timer();
+  socket.on("2_to_4_send-req-to-driver", msg => {
+    const { reqId, lat, lng, addr } = JSON.parse(msg);
+    if (reqId === undefined || lat === undefined || lng === undefined) return;
 
-        // start đồng hồ
-        if (timer.isRunning() == false) {
-            timer.start({
-                countdown: true,
-                startValues: {
-                    seconds: 10
-                }
-            });
-            $("#countdownExample #timer-value").html(timer.getTimeValues().seconds);
-            timer.addEventListener("secondsUpdated", function (e) {
-                $("#countdownExample #timer-value").html(timer.getTimeValues().seconds);
-                // khi click button chấp nhận
-                $('#btn-accept').click(() => {
-                    // update trạng thái của request dưới db
-                    updateReqStatus(reqId);
-
-                    // update lại map
-                    updateMap(lat, lng, addr);
-
-                    // TODO: GỌI ajax cập nhật tọa độ driver phụ trách request đó
-
-                    // Mở button Đón khách lên
-                    $('#btn-take').prop("disabled", false);
-                })
-
-                $('#btn-reject').click(() => {
-                    timer.stop();
-                    // timer.reset();
-                    $("#requestModalCenter").modal("hide");
-                })
-            });
-
-            timer.addEventListener("targetAchieved", function (e) {
-                $("#countdownExample #timer-value")
-                    .html("Không phản hồi")
-                    .addClass("timer-timeout");
-                setTimeout(function () {
-                    $("#requestModalCenter").modal("hide");
-                    $("#countdownExample #timer-value").removeClass("timer-timeout");
-                }, 500);
-            });
-
-            // hiện modal accept
-            $("#requestModalCenter").modal("show");
+    // start đồng hồ
+    if (timer.isRunning() == false) {
+      timer.start({
+        countdown: true,
+        startValues: {
+          seconds: 10
         }
-    });
+      });
+      $("#countdownExample #timer-value").html(timer.getTimeValues().seconds);
+      timer.addEventListener("secondsUpdated", function(e) {
+        $("#countdownExample #timer-value").html(timer.getTimeValues().seconds);
+        // khi click button chấp nhận
+        $("#btn-accept").click(() => {
+          // update trạng thái của request dưới db
+          updateReqStatus(reqId);
+
+          // update lại map
+          updateMap(lat, lng, addr);
+
+          // TODO: GỌI ajax cập nhật tọa độ driver phụ trách request đó
+
+          // Mở button Đón khách lên
+          $("#btn-take").prop("disabled", false);
+        });
+
+        $("#btn-reject").click(() => {
+          timer.stop();
+          // timer.reset();
+          $("#requestModalCenter").modal("hide");
+        });
+      });
+
+      timer.addEventListener("targetAchieved", function(e) {
+        $("#countdownExample #timer-value")
+          .html("Không phản hồi")
+          .addClass("timer-timeout");
+        setTimeout(function() {
+          $("#requestModalCenter").modal("hide");
+          $("#countdownExample #timer-value").removeClass("timer-timeout");
+        }, 500);
+      });
+
+      // hiện modal accept
+      $("#requestModalCenter").modal("show");
+    }
+  });
 });
 //socket end
 
-$(function () {
-    // khi click button Đón Khách
-    $('#btn-take').click(() => {
-        // tự disable chính mình
-        $('#btn-take').prop("disabled", true);
-        // mở nút Bắt đầu lên
-        $('#btn-start').prop("disabled", false);
-        // chuyển trạng thái thành BUSY
-        changeStatus(DRIVER_STATUS_BUSY);
-        // disable lun trạng thái, ko cho sửa
-        $("#navbarDropdown").prop("disabled", true);
+$(function() {
+  // khi click button Đón Khách
+  $("#btn-take").click(() => {
+    // tự disable chính mình
+    $("#btn-take").prop("disabled", true);
+    // mở nút Bắt đầu lên
+    $("#btn-start").prop("disabled", false);
+    // chuyển trạng thái thành BUSY
+    changeStatus(DRIVER_STATUS_BUSY);
+    // disable lun trạng thái, ko cho sửa
+    $("#navbarDropdown").prop("disabled", true);
 
-        // TODO: xử lý marker driver nhảy vị trí tới marker khách
+    // TODO: xử lý marker driver nhảy vị trí tới marker khách
+    passengerMarker.setMap(null);
+    driverMarker.setMap(null);
+    // TODO: lúc này số lượng marker giảm còn 1 marker -> đổi marker thành chiếc xe đèo khách -> moving.png
+    driverMarker = new google.maps.Marker({
+      position: passengerLatLng,
+      map,
+      icon: "../../assets/img/moving.png",
+      draggable: false,
+      animation: google.maps.Animation.BOUNCE
+    });
+  });
 
-        // TODO: lúc này số lượng marker giảm còn 1 marker -> đổi marker thành chiếc xe đèo khách -> moving.png
+  // khi click button Bắt Đầu
+  $("#btn-start").click(() => {
+    // tự disable chính mình
+    $("#btn-start").prop("disabled", true);
 
+    // Ấn bắt đầu thì sẽ làm gì????
+    // TODO: chọn địa điểm tới tùy ý
+    // -> bằng cách kéo thả marker trên map rồi confirm yes/no bằng infoWindow -> giống App#2
+    // driverMarker draggable
+    driverMarker.setMap(null);
+    driverMarker = new google.maps.Marker({
+      position: passengerLatLng,
+      map,
+      icon: "../../assets/img/moving.png",
+      draggable: true
+    });
+    driverMarker.addListener("mouseup", function() {
+      if (infoWindow) infoWindow.close();
+      const desLat = driverMarker.getPosition().lat(),
+        desLng = driverMarker.getPosition().lng();
+      $.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${desLat},${desLng}&location_type=ROOFTOP&result_type=street_address&key=AIzaSyDas6_Z8AZ6sdYJGOucYDWh-MCcoB9jjVE`,
+        function(data) {
+          infoWindow = new google.maps.InfoWindow({
+            content: `
+                <div class="infowindow-container">
+                  <p id="infowindow-address">${
+                    data.status === "OK"
+                      ? data.results[0].formatted_address
+                      : ""
+                  }</p>
+                  <p>Di chuyển đến vị trí này?</p>
+                  <div class="infowindow-btn btn-group">
+                    <button class="btn btn-success" 
+                      onClick="document.getElementById('acceptDestination').click()">
+                      Có
+                    </button>
+                    <button class="btn btn-cancel" 
+                      onClick="document.getElementById('declineDestination').click()">
+                      Điều chỉnh tiếp
+                    </button>
+                  </div>
+                </div>`
+          });
+          infoWindow.open(map, driverMarker);
+        }
+      );
+    });
+  });
 
-    })
+  // khi click button Kết Thúc
+  $("#btn-finish").click(() => {
+    // tự disable chính mình
+    $("#btn-finish").prop("disabled", true);
 
-    // khi click button Bắt Đầu
-    $('#btn-start').click(() => {
-        // tự disable chính mình
-        $('#btn-start').prop("disabled", true);
+    // TODO: xử lý trạng thái: chuyển lại thành READY
+    // enable lại trạng thái
+    $("#navbarDropdown").prop("disabled", false);
+    // chuyển thành READY
+    changeStatus(DRIVER_STATUS_READY);
+    // reset map
+    initMap();
+  });
+});
 
-        // Ấn bắt đầu thì sẽ làm gì????
-        // TODO: chọn địa điểm tới tùy ý 
-        // -> bằng cách kéo thả marker trên map rồi confirm yes/no bằng infoWindow -> giống App#2
+$(function() {
+  $("#acceptDestination").click(function() {
+    infoWindow.close();
+    // Nếu là yes -> đã tới địa điểm thương lượng -> thì mới mở nút Kết thúc lên để end chuyến đi
+    // mở nút Kết Thúc lên
+    $("#btn-finish").prop("disabled", false); // chỉ khi ấn nút Yes của infoWindow thì mới mở lên});
+  });
 
-        
-        // Nếu là yes -> đã tới địa điểm thương lượng -> thì mới mở nút Kết thúc lên để end chuyến đi
-        // mở nút Kết Thúc lên
-        // $('#btn-finish').prop("disabled", false); // chỉ khi ấn nút Yes của infoWindow thì mới mở lên
-
-    })
-
-    // khi click button Kết Thúc
-    $('#btn-finish').click(() => {
-        // tự disable chính mình
-        $('#btn-finish').prop("disabled", true);
-
-        // TODO: xử lý trạng thái: chuyển lại thành READY
-        // enable lại trạng thái
-        $("#navbarDropdown").prop("disabled", false);
-        // chuyển thành READY
-        changeStatus(DRIVER_STATUS_READY);
-
-    })
-})
+  $("#declineDestination").click(function() {
+    infoWindow.close();
+  });
+});
--- requestbike_frontend/src/4_driver/google-map/script.js
+++ requestbike_frontend/src/4_driver/google-map/script.js
@@ -1,220 +1,217 @@
 // DECLARE VARIABLES AND FUNCTIONS =======================================
 // @circle indicates the area <= 100 meters around user's current position
 let map = null,
-    driverMarker = null,
-    passengerMarker = null,
-    passengerLatLng = null,
-    circle = null,
-    pathLine = null,
-    prevLatLng = null;
+  driverMarker = null,
+  passengerMarker = null,
+  passengerLatLng = null,
+  circle = null,
+  pathLine = null,
+  prevLatLng = null;
 
 /// google map start ==========================================================
 
 function getNewDriverMarkerLatLng() {
-    return {
-        lat: driverMarker.getPosition().lat(),
-        lng: driverMarker.getPosition().lng()
-    };
+  return {
+    lat: driverMarker.getPosition().lat(),
+    lng: driverMarker.getPosition().lng()
+  };
 }
 
 function getPrevDriverMarkerLatLng() {
-    return {
-        lat: prevLatLng.lat(),
-        lng: prevLatLng.lng()
-    };
+  return {
+    lat: prevLatLng.lat(),
+    lng: prevLatLng.lng()
+  };
 }
 
 function drawDriverMarker(latLng) {
-    // remove map attached and listeners
-    if (driverMarker) {
-        driverMarker.setMap(null);
-        google.maps.event.clearListeners(driverMarker, "mouseup");
-        google.maps.event.clearListeners(driverMarker, "mousedown");
-    }
-    // draw new marker
-    driverMarker = new google.maps.Marker({
-        position: latLng,
-        map,
-        icon: "../../assets/img/driver.png",
-        draggable: true,
-        animation: google.maps.Animation.BOUNCE
-    });
-    // (re-)add event listeners
-    driverMarker.addListener("mouseup", moveDriverMarkerMouseUp);
-    driverMarker.addListener("mousedown", moveDriverMarkerMouseDown);
+  // remove map attached and listeners
+  if (driverMarker) {
+    driverMarker.setMap(null);
+    google.maps.event.clearListeners(driverMarker, "mouseup");
+    google.maps.event.clearListeners(driverMarker, "mousedown");
+  }
+  // draw new marker
+  driverMarker = new google.maps.Marker({
+    position: latLng,
+    map,
+    icon: "../../assets/img/driver.png",
+    draggable: true,
+    animation: google.maps.Animation.BOUNCE
+  });
+  // (re-)add event listeners
+  driverMarker.addListener("mouseup", moveDriverMarkerMouseUp);
+  driverMarker.addListener("mousedown", moveDriverMarkerMouseDown);
 }
 
 function drawDriverCircle(opts) {
-    // remove map attached and listeners
-    if (circle) {
-        circle.setMap(null);
-    }
-    // draw new circle
-    circle = new google.maps.Circle({
-        strokeColor: opts.strokeColor,
-        stokeOpacity: opts.stokeOpacity,
-        strokeWeight: opts.strokeWeight,
-        fillColor: opts.fillColor,
-        fillOpacity: opts.fillOpacity,
-        map,
-        center: opts.center,
-        radius: 100
-    });
+  // remove map attached and listeners
+  if (circle) {
+    circle.setMap(null);
+  }
+  // draw new circle
+  circle = new google.maps.Circle({
+    strokeColor: opts.strokeColor,
+    stokeOpacity: opts.stokeOpacity,
+    strokeWeight: opts.strokeWeight,
+    fillColor: opts.fillColor,
+    fillOpacity: opts.fillOpacity,
+    map,
+    center: opts.center,
+    radius: 100
+  });
 }
 
 function drawPathDriverToPassenger(driverLatLng, passengerLatLng) {
-    var directionsService = new google.maps.DirectionsService;
-    const DirectionsRequest = {
-        origin: driverLatLng,
-        destination: passengerLatLng,
-        travelMode: 'DRIVING'
-    }
-    directionsService.route(DirectionsRequest, function (result, status) {
-        if (status == 'OK') {
-            //todo
-            directionsDisplay = new google.maps.DirectionsRenderer({
-                directions: result,
-                map: map,
-                markerOptions: {
-                    visible: false
-                }
-            });
+  var directionsService = new google.maps.DirectionsService();
+  const DirectionsRequest = {
+    origin: driverLatLng,
+    destination: passengerLatLng,
+    travelMode: "DRIVING"
+  };
+  directionsService.route(DirectionsRequest, function(result, status) {
+    if (status == "OK") {
+      //todo
+      directionsDisplay = new google.maps.DirectionsRenderer({
+        directions: result,
+        map: map,
+        markerOptions: {
+          visible: false
         }
-    })
+      });
+    }
+  });
 }
 
 function showDialog(content = "Something happened") {
-    const dialog = document.getElementById("dialog");
-    if (dialog) {
-        dialog.innerHTML = content;
-        dialog.style.display = "block";
-        window.setTimeout(() => {
-            dialog.style.display = "none";
-        }, 2000);
-    }
+  const dialog = document.getElementById("dialog");
+  if (dialog) {
+    dialog.innerHTML = content;
+    dialog.style.display = "block";
+    window.setTimeout(() => {
+      dialog.style.display = "none";
+    }, 2000);
+  }
 }
 
 let timer = null;
 // change circle colors to notify if user had move marker too far
 function moveDriverMarkerMouseDown() {
-    let isOutCircle = false;
+  let isOutCircle = false;
 
-    timer = setInterval(function () {
-        const _newLagLng = getNewDriverMarkerLatLng(),
-            _prevLatLng = getPrevDriverMarkerLatLng();
-        if (Haversine(_prevLatLng, _newLagLng) > 100) {
-            if (isOutCircle === false) {
-                isOutCircle = true;
-                drawDriverCircle({
-                    strokeColor: "#fc3232",
-                    stokeOpacity: 0.8,
-                    strokeWeight: 2,
-                    fillColor: "#ff7575",
-                    fillOpacity: 0.2,
-                    center: prevLatLng
-                });
-            }
-        } else {
-            if (isOutCircle === true) {
-                isOutCircle = false;
-                drawDriverCircle({
-                    strokeColor: "#4286f4",
-                    stokeOpacity: 0.8,
-                    strokeWeight: 2,
-                    fillColor: "#93bcff",
-                    fillOpacity: 0.2,
-                    center: prevLatLng
-                });
-            }
-        }
-    }, 100);
+  timer = setInterval(function() {
+    const _newLagLng = getNewDriverMarkerLatLng(),
+      _prevLatLng = getPrevDriverMarkerLatLng();
+    if (Haversine(_prevLatLng, _newLagLng) > 100) {
+      if (isOutCircle === false) {
+        isOutCircle = true;
+        drawDriverCircle({
+          strokeColor: "#fc3232",
+          stokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: "#ff7575",
+          fillOpacity: 0.2,
+          center: prevLatLng
+        });
+      }
+    } else {
+      if (isOutCircle === true) {
+        isOutCircle = false;
+        drawDriverCircle({
+          strokeColor: "#4286f4",
+          stokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: "#93bcff",
+          fillOpacity: 0.2,
+          center: prevLatLng
+        });
+      }
+    }
+  }, 100);
 }
 
 // if the distance between new moved and previous marker is
 // <= 100, move marker to the new position
 // otherwise, alert user and revert the marker to previous position
 function moveDriverMarkerMouseUp() {
-    clearInterval(timer);
-    const _newLagLng = getNewDriverMarkerLatLng(),
-        _prevLatLng = getPrevDriverMarkerLatLng();
+  clearInterval(timer);
+  const _newLagLng = getNewDriverMarkerLatLng(),
+    _prevLatLng = getPrevDriverMarkerLatLng();
 
-    if (Haversine(_prevLatLng, _newLagLng) > 100) {
-        // show alert
-        showDialog("Vị trị mới không cách vị trí gốc quá 100m");
-        // revert marker position
-        drawDriverMarker(prevLatLng);
-        // revert map center
-        map.panTo(prevLatLng);
-        // draw new circle
-        drawDriverCircle({
-            strokeColor: "#4286f4",
-            stokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: "#93bcff",
-            fillOpacity: 0.2,
-            center: prevLatLng
-        });
-        // draw new path
-    } else { // do nothing
-
-    }
+  if (Haversine(_prevLatLng, _newLagLng) > 100) {
+    // show alert
+    showDialog("Vị trị mới không cách vị trí gốc quá 100m");
+    // revert marker position
+    drawDriverMarker(prevLatLng);
+    // revert map center
+    map.panTo(prevLatLng);
+    // draw new circle
+    drawDriverCircle({
+      strokeColor: "#4286f4",
+      stokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#93bcff",
+      fillOpacity: 0.2,
+      center: prevLatLng
+    });
+    // draw new path
+  } else {
+    // do nothing
+  }
 }
 
 function handleQueryGeolocationFinish(pos) {
-    // extract position
-    const {
-        latitude,
-        longitude
-    } = pos.coords;
-    const initialDriverPos = new google.maps.LatLng(latitude, longitude);
-    // add marker indicating user position
-    drawDriverMarker(initialDriverPos);
-    // save the initial position for later use
-    // prevLatLng = new google.maps.LatLng(latitude, longitude);
-    prevLatLng = initialDriverPos;
-    // draw circle
-    drawDriverCircle({
-        strokeColor: "#4286f4",
-        stokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#93bcff",
-        fillOpacity: 0.2,
-        center: initialDriverPos
-    });
-    // re-config the map
-    map.setZoom(DEFAULT_ZOOM_LEVEL);
-    map.panTo(initialDriverPos);
-    // set the map to use TrafficLayer
-    // ? should turn this layer on before the journey started
-    // const trafficLayer = new google.maps.TrafficLayer();
-    // trafficLayer.setMap(map);
+  // extract position
+  const { latitude, longitude } = pos.coords;
+  const initialDriverPos = new google.maps.LatLng(latitude, longitude);
+  // add marker indicating user position
+  drawDriverMarker(initialDriverPos);
+  // save the initial position for later use
+  // prevLatLng = new google.maps.LatLng(latitude, longitude);
+  prevLatLng = initialDriverPos;
+  // draw circle
+  drawDriverCircle({
+    strokeColor: "#4286f4",
+    stokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#93bcff",
+    fillOpacity: 0.2,
+    center: initialDriverPos
+  });
+  // re-config the map
+  map.setZoom(DEFAULT_ZOOM_LEVEL);
+  map.panTo(initialDriverPos);
+  // set the map to use TrafficLayer
+  // ? should turn this layer on before the journey started
+  // const trafficLayer = new google.maps.TrafficLayer();
+  // trafficLayer.setMap(map);
 }
 
 function initMap() {
-    // find target to hold the map
-    const targetDivMap = document.getElementById("map");
-    if (!targetDivMap) {
-        return;
-    }
-    // initialize the map
-    map = new google.maps.Map(targetDivMap, {
-        zoom: INIT_ZOOM_LEVEL,
-        center: new google.maps.LatLng(10.762622, 106.660172) // HCM
-    });
-    // query user geolocation
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(handleQueryGeolocationFinish);
-    }
+  // find target to hold the map
+  const targetDivMap = document.getElementById("map");
+  if (!targetDivMap) {
+    return;
+  }
+  // initialize the map
+  map = new google.maps.Map(targetDivMap, {
+    zoom: INIT_ZOOM_LEVEL,
+    center: new google.maps.LatLng(10.762622, 106.660172) // HCM
+  });
+  // query user geolocation
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(handleQueryGeolocationFinish);
+  }
 }
 
 function drawPassengerMarker(latLng) {
-    passengerMarker = new google.maps.Marker({
-        position: latLng,
-        map,
-        icon: "../../assets/img/man.png",
-        animation: google.maps.Animation.BOUNCE,
-        draggable: false
-    });
+  passengerMarker = new google.maps.Marker({
+    position: latLng,
+    map,
+    icon: "../../assets/img/man.png",
+    animation: google.maps.Animation.BOUNCE,
+    draggable: false
+  });
 }
 
 // google map end =============================================================
